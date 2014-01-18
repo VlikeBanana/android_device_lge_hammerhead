71e93b0392abcb82798d1c1566d974e1b728da91
VlikeBanana <AndrolinRoot@googlemail.com>
2014-01-18 12:40:32 -0500

    IllusionRom

--- ill.mk
+++ ill.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2012 The CyanogenMod Project
-# Copyright (C) 2012 The Carbon Project
+# Copyright (C) 2014 IllusionRoms
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -29,18 +29,22 @@ $(call inherit-product-if-exists, device/lge/hammerhead/full_hammerhead.mk)
 PRODUCT_LOCALES := en_US de_DE zh_CN zh_TW cs_CZ nl_BE nl_NL en_AU en_GB en_CA en_NZ en_SG fr_BE fr_CA fr_FR fr_CH de_AT de_LI de_CH it_IT it_CH ja_JP ko_KR pl_PL ru_RU es_ES ar_EG ar_IL bg_BG ca_ES hr_HR da_DK en_IN en_IE en_ZA fi_FI el_GR iw_IL hi_IN hu_HU in_ID lv_LV lt_LT nb_NO pt_BR pt_PT ro_RO sr_RS sk_SK sl_SI es_US sv_SE tl_PH th_TH tr_TR uk_UA vi_VN
 
 # phone
-$(call inherit-product, vendor/carbon/config/common_phone.mk)
+$(call inherit-product, vendor/illusion/config/common_phone.mk)
 
 # products
 PRODUCT_DEVICE := hammerhead
 PRODUCT_BRAND := google
-PRODUCT_NAME := carbon_hammerhead
+PRODUCT_NAME := ill_hammerhead
 PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
-PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=carbon.hammerhead.$(shell date +%m%d%y).$(shell date +%H%M%S)
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # overrides
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
     PRIVATE_BUILD_DESC="hammerhead-user 4.4.2 KOT49H 937116 release-keys" \
     BUILD_FINGERPRINT="google/hammerhead/hammerhead:4.4.2/KOT49H/937116:user/release-keys"
+
+# bootanimation
+PRODUCT_COPY_FILES += \
+        vendor/illusion/bootanimation/Illusion800x800.zip:system/media/bootanimation.zip
